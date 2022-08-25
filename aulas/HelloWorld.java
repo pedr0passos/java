258939c61d3de511914b3dfedcac6876eefd1ec5
Pedro Passos <102426718+pedr0passos@users.noreply.github.com>
2022-08-25 11:11:23 -0300

    java

---
+++ aulas/HelloWorld.java
@@ -0,0 +1,12 @@
+package aulas;
+
+import java.util.Date;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é");
+        System.out.println(relogio.toString());
+    }
+}
+
--- aulas/teste.java
+++
@@ -1,7 +0,0 @@
-package aulas;
-
-public class teste {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
