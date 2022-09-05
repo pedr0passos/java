b86fecac8e41dd52c4512803dad53179a22fa121
Pedro Passos <102426718+pedr0passos@users.noreply.github.com>
2022-09-05 12:34:01 -0300

    aula-prod

---
+++ aulas/a002/Produto.java
@@ -0,0 +1,28 @@
+package aulas.a002;
+
+import java.util.Date;
+
+public class Produto {
+
+    // Propriedades
+    int codigo;
+    String nome;
+    float preco;
+    Date dataFabricacao;
+    String fabricante;
+
+    //Métodos
+    public float calcImposto(int taxa) {
+        float imposto;
+        if ( taxa > 15 ) {
+            imposto = preco * 0.23;
+        } else {
+            imposto = preco * 0.16;
+        }
+        return imposto;
+    }
+}
+
+public class ProgramaPrincipal {
+    
+}
---
+++ aulas/a002/ProgramaPrincipal.java
@@ -0,0 +1,11 @@
+package aulas.a002;
+
+public class ProgramaPrincipal {
+    public static void main(String[] args) {
+        Produto p1 = new Produto();
+        p1.nome = "Feijão";
+        p1.codigo = 123456;
+        p1.dataFabricacao = 1990-02-15;
+
+    }
+}
